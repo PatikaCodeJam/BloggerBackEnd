6dc94e9b10136811d9a46efd0ba07e3ce0931ad3
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-24 17:21:04 +0300

    Add @CrossOrigin

--- src/main/java/com/blogger/platform/api/controllers/BlogsController.java
+++ src/main/java/com/blogger/platform/api/controllers/BlogsController.java
@@ -4,12 +4,12 @@ import com.blogger.platform.business.abstracts.BlogService;
 import com.blogger.platform.core.utilities.results.DataResult;
 import com.blogger.platform.core.utilities.results.Result;
 import com.blogger.platform.entities.concretes.Blog;
-import com.blogger.platform.entities.concretes.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/blogs/")
 public class BlogsController {
--- src/main/java/com/blogger/platform/api/controllers/UsersController.java
+++ src/main/java/com/blogger/platform/api/controllers/UsersController.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/users/")
 public class UsersController {
