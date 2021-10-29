8b33e638889968f3a49611f3eb33ac4ce3aa7a94
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-29 16:27:55 +0300

    Delete UserFollowerDao.java

--- src/main/java/com/blogger/platform/dataAccess/abstracts/UserFollowerDao.java
+++
@@ -1,8 +0,0 @@
-package com.blogger.platform.dataAccess.abstracts;
-
-public interface UserFollowerDao {
-//public interface UserFollowerDao extends JpaRepository<UserFollower, Integer> {
-    /*UserFollower getByUserFollowerId(int userFollowerId);
-
-    UserFollower getByUser_UserId(int userId);*/
-}
